e44850d3b5a7a58f20d47cee844e09b61165e03c
Gemut <Gemut@MacBook-Pro-Gemut.local>
2024-12-12 23:56:47 +0300

    renew lucky_ticket

--- lucky_ticket.js
+++ lucky_ticket.js
@@ -1,8 +1,12 @@
 function checkTicket(number) {
   const numbers = number.split("").map(Number);
 
-  const firstSumOfNum = numbers.slice(0, 3).reduce((acc, num) => acc + num, 0);
-  const secondSumOfNum = numbers.slice(3).reduce((acc, num) => acc + num, 0);
+  const firstSumOfNum = numbers
+    .slice(0, numbers.length / 2)
+    .reduce((acc, num) => acc + num, 0);
+  const secondSumOfNum = numbers
+    .slice(numbers.length / 2, number)
+    .reduce((acc, num) => acc + num, 0);
 
   return firstSumOfNum === secondSumOfNum;
 }
